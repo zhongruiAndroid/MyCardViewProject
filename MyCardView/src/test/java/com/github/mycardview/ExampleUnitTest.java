7037078d7f6978470e6132f56e1cbcf7e4ab3036
Administrator <zr2013123@sina.com>
2019-08-21 18:05:12 +0800

    file

--- MyCardView/src/test/java/com/github/mycardview/ExampleUnitTest.java
+++ MyCardView/src/test/java/com/github/mycardview/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.github.mycardview;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -19,5 +22,49 @@ public class ExampleUnitTest {
         public Person() {
             this.sex = "男";
         }
+    }
+
+
+    @Test
+    public void asddf() {
+        List<String> list=new ArrayList<>();
+        list.add("*.iml");
+        list.add("*.apk");
+        list.add(".gradle/");
+        list.add(".idea/");
+        list.add("app/*.iml");
+        list.add("build/");
+        list.add("gradle/");
+        list.add("mvp/build/");
+        list.add("mvp/*.iml");
+        list.add("RecyclerBanner/build/");
+        list.add("RecyclerBanner/*.iml");
+        list.add("service/build/");
+        list.add("service/*.iml");
+        list.add("umlibrary/build/");
+        list.add("umlibrary/*.iml");
+        list.add("v2/build/");
+        list.add("v2/*.iml");
+        list.add("v2/*.apk");
+        list.add("zssqservice/build/");
+        list.add("zssqservice/*.iml");
+        list.add("local.properties");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("<ignored path=\""+list.get(i)+"\" />");
+        }
+    }
+    @Test
+    public void asdf() {
+        String a="ab";
+        String b="a"+"b";
+        String c=new String("ab");
+        String cd=new String("ab");
+        String aa="a";
+        String d=aa+"b";
+        System.out.println(a==b);
+        System.out.println(a==c);
+        System.out.println(aa==d);
+        System.out.println(cd==c);
+        System.out.println(cd.equals(c));
     }
 }
